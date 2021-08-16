bed6f715d8360756a4e7781b81b3f9bbb6bbc45c
Norwick Kin On Lee <norwick.ko.lee@gmail.com>
2021-08-16 11:46:43 -0700

    junit simple

---
+++ src/test/java/jenkinstry/JenkinsTryTest.java
@@ -0,0 +1,13 @@
+package jenkinstry;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class JenkinsTryTest {
+
+	@Test
+	void test() {
+		Assertions.assertTrue(true);
+	}
+
+}
